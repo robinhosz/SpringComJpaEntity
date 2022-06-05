a7ef7a88fc2a121e4ab001d9fac11bd8b8a73583
José Robson Siqueira Júnior <jrobson@server01.foursys.local>
2022-06-05 00:21:49 -0300

    jpa puro

---
+++ src/main/java/br/com/coursespring/fourcamp/model/Cliente.java
@@ -0,0 +1,62 @@
+package br.com.coursespring.fourcamp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "nome", length = 100)
+	private String nome;
+
+	public Cliente() {
+		
+	}
+	
+	
+	
+	public Cliente(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+
+
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + "]";
+	}
+
+}
